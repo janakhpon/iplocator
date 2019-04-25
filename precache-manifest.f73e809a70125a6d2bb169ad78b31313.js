884d565897b41c14cd0ec00987adad713f8bd6bf
janakhpon <jnovaxer@gmail.com>
2019-04-26 04:49:24 +0630

    Updates

---
+++ precache-manifest.f73e809a70125a6d2bb169ad78b31313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db707d4bd625a5bc74d3",
+    "url": "/iplocator/static/js/runtime~main.db707d4b.js"
+  },
+  {
+    "revision": "c5808232a1e33f72c2c2",
+    "url": "/iplocator/static/js/main.c5808232.chunk.js"
+  },
+  {
+    "revision": "0a944bbc92d6ca5dcc26",
+    "url": "/iplocator/static/js/1.0a944bbc.chunk.js"
+  },
+  {
+    "revision": "c5808232a1e33f72c2c2",
+    "url": "/iplocator/static/css/main.095e4efc.chunk.css"
+  },
+  {
+    "revision": "ed77d7ca0b578e574d5f145df42cd76f",
+    "url": "/iplocator/index.html"
+  }
+];
